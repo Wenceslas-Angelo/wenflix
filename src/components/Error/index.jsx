8baab3eec4d0873800de61823109fcfd372eced0
Wenceslas Angelo <wenceslasangelo@gmail.com>
2023-04-17 15:33:05 +0300

    error 404

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Error() {
-  return <div className="error">Error</div>;
+  return (
+    <div className="error">
+      <h1>Error</h1>
+    </div>
+  );
 }
 
 export default Error;
